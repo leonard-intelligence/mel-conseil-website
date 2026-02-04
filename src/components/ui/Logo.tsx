312506689cd4f33dcdb9f391707026668d11e6c2
hypercube92 <guillaume3301@proton.me>
2026-02-04 22:31:27 +0100

    chore: Update logo image source path.

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,8 +1,6 @@
-import symbol from '/assets/logos/leonard-logo-white.png';
-
 export const Logo = ({ className }: { className?: string }) => (
   <div className={`flex items-center gap-3 ${className}`}>
-    <img src={symbol} alt="Leonard Intelligence" className="h-8 w-auto mb-1" />
+    <img src="/logo_white.png" alt="Leonard Intelligence" className="h-8 w-auto mb-1" />
     <div className="flex items-baseline gap-2 leading-none text-white">
       <span className="font-jacquard text-3xl font-normal">Leonard</span>
       <span className="font-junicode italic text-3xl">Intelligence</span>
